affdefdb2ae45cad12a8b34886b11460e28ba117
krapnik <krapnik@qq.com>
2022-08-21 23:24:27 +0800

    update

---
+++ 1_basic/7.5_camera_exercise1/main.js
@@ -0,0 +1,205 @@
+async function main() {
+    const gl = document.getElementById("canvas").getContext("webgl2");
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    let shader = new Shader(gl, "shader.vs", "shader.fs");
+    await shader.initialize();
+
+    let cameraPos = glMatrix.vec3.fromValues(0.0, 0.0, 3.0);
+    let camera = new Camera(cameraPos);
+
+    // timing
+    let deltaTime = 0.0;	// time between current frame and last frame
+    let lastFrame = 0.0;
+    let isFirstMouse = true;
+    let lastX = gl.drawingBufferWidth / 2, lastY = gl.drawingBufferHeight / 2;
+
+
+    document.onkeydown = (e) => {
+        camera.onKeydown(e.code, deltaTime);
+        camera.position[1] = 0;
+    }
+
+    canvas.onmousemove = (e) => {
+        let { clientX, clientY } = e;
+        if (isFirstMouse) {
+            lastX = clientX;
+            lastY = clientY;
+            isFirstMouse = false;
+        }
+        let offsetX = clientX - lastX;
+        let offsetY = lastY - clientY;
+
+        lastX = clientX;
+        lastY = clientY;
+
+        camera.onMousemove(offsetX, offsetY);
+    }
+
+    canvas.onwheel = (e) => {
+        camera.onMouseScroll(e.deltaY / 100);
+    }
+
+    let vertices = new Float32Array([
+        -0.5, -0.5, -0.5, 0.0, 0.0,
+        0.5, -0.5, -0.5, 1.0, 0.0,
+        0.5, 0.5, -0.5, 1.0, 1.0,
+        0.5, 0.5, -0.5, 1.0, 1.0,
+        -0.5, 0.5, -0.5, 0.0, 1.0,
+        -0.5, -0.5, -0.5, 0.0, 0.0,
+
+        -0.5, -0.5, 0.5, 0.0, 0.0,
+        0.5, -0.5, 0.5, 1.0, 0.0,
+        0.5, 0.5, 0.5, 1.0, 1.0,
+        0.5, 0.5, 0.5, 1.0, 1.0,
+        -0.5, 0.5, 0.5, 0.0, 1.0,
+        -0.5, -0.5, 0.5, 0.0, 0.0,
+
+        -0.5, 0.5, 0.5, 1.0, 0.0,
+        -0.5, 0.5, -0.5, 1.0, 1.0,
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+        -0.5, -0.5, 0.5, 0.0, 0.0,
+        -0.5, 0.5, 0.5, 1.0, 0.0,
+
+        0.5, 0.5, 0.5, 1.0, 0.0,
+        0.5, 0.5, -0.5, 1.0, 1.0,
+        0.5, -0.5, -0.5, 0.0, 1.0,
+        0.5, -0.5, -0.5, 0.0, 1.0,
+        0.5, -0.5, 0.5, 0.0, 0.0,
+        0.5, 0.5, 0.5, 1.0, 0.0,
+
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+        0.5, -0.5, -0.5, 1.0, 1.0,
+        0.5, -0.5, 0.5, 1.0, 0.0,
+        0.5, -0.5, 0.5, 1.0, 0.0,
+        -0.5, -0.5, 0.5, 0.0, 0.0,
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+
+        -0.5, 0.5, -0.5, 0.0, 1.0,
+        0.5, 0.5, -0.5, 1.0, 1.0,
+        0.5, 0.5, 0.5, 1.0, 0.0,
+        0.5, 0.5, 0.5, 1.0, 0.0,
+        -0.5, 0.5, 0.5, 0.0, 0.0,
+        -0.5, 0.5, -0.5, 0.0, 1.0
+    ]);
+
+    let cubePositions = [
+        glMatrix.vec3.fromValues(0.0, 0.0, 0.0),
+        glMatrix.vec3.fromValues(2.0, 5.0, -15.0),
+        glMatrix.vec3.fromValues(-1.5, -2.2, -2.5),
+        glMatrix.vec3.fromValues(-3.8, -2.0, -12.3),
+        glMatrix.vec3.fromValues(2.4, -0.4, -3.5),
+        glMatrix.vec3.fromValues(-1.7, 3.0, -7.5),
+        glMatrix.vec3.fromValues(1.3, -2.0, -2.5),
+        glMatrix.vec3.fromValues(1.5, 2.0, -2.5),
+        glMatrix.vec3.fromValues(1.5, 0.2, -1.5),
+        glMatrix.vec3.fromValues(-1.3, 1.0, -1.5)
+    ]
+
+    let positionLoc = 0, texCoordLoc = 1;
+
+    let vao = gl.createVertexArray();
+    let vbo = gl.createBuffer();
+
+    gl.bindVertexArray(vao);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, gl.FALSE, 5 * vertices.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, gl.FALSE, 5 * vertices.BYTES_PER_ELEMENT, 3 * vertices.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(texCoordLoc);
+
+    let texture1 = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture1);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+
+    let img1 = await loadImage("../../resources/textures/container.jpg");
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img1);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+
+    let texture2 = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture2);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    let img2 = await loadImage("../../resources/textures/awesomeface.png");
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img2);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    shader.use();
+    gl.uniform1i(gl.getUniformLocation(shader.ID, "texture1"), 0);
+    shader.setInt("texture2", 1);
+
+    let projectionLoc = gl.getUniformLocation(shader.ID, "projection");
+    let modelLoc = gl.getUniformLocation(shader.ID, "model");
+    let viewLoc = gl.getUniformLocation(shader.ID, "view");
+    shader.use();
+
+    let projection = glMatrix.mat4.create();
+
+    function render(time) {
+        let currentFrame = Math.round(time) / 1000;
+        deltaTime = Math.floor(currentFrame * 1000 - lastFrame * 1000) / 1000;
+        lastFrame = currentFrame;
+        gl.clearColor(0.2, 0.3, 0.3, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture1);
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, texture2);
+        gl.bindVertexArray(vao);
+
+        let view = camera.getViewMatrix();
+        gl.uniformMatrix4fv(viewLoc, false, view);
+
+        glMatrix.mat4.perspective(projection, glMatrix.glMatrix.toRadian(camera.zoom), gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 100)
+        gl.uniformMatrix4fv(projectionLoc, false, projection);
+
+        for (let i = 0; i < cubePositions.length; i++) {
+            let model = glMatrix.mat4.create();
+            glMatrix.mat4.translate(model, model, cubePositions[i]);
+            let angle = 20 * i;
+            if (i % 2 == 0) {
+                angle = time / 1000 * 25;
+            }
+            glMatrix.mat4.rotate(model, model, glMatrix.glMatrix.toRadian(angle), glMatrix.vec3.fromValues(1.0, 0.3, 0.5));
+            gl.uniformMatrix4fv(modelLoc, false, model);
+            gl.drawArrays(gl.TRIANGLES, 0, 36);
+        }
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+}
+
+async function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        let image = new Image();
+        image.src = url;
+        image.onload = () => {
+            resolve(image);
+        }
+        image.onerror = (err) => {
+            reject(err);
+            throw new Error(err);
+        }
+
+    })
+}
---
+++ libs/camera.js
@@ -0,0 +1,106 @@
+let isFirstMouse = true;
+let lastX, lastY;
+const YAW = -90.0;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+const PITCH = 0.0;
+const ZOOM = 45.0;
+const SENSITIVITY = 0.1;
+const SPEED = 5;
+
+class Camera {
+    position;
+    front;
+    up = glMatrix.vec3.create();
+    right = glMatrix.vec3.create();
+    worldUp;
+    yaw;
+    pitch;
+    // camera options
+    movementSpeed;
+    mouseSensitivity;
+    zoom;
+    constructor(position = glMatrix.vec3.fromValues(0, 0, 0), up = glMatrix.vec3.fromValues(0, 1, 0), yaw = YAW, pitch = PITCH) {
+        this.front = glMatrix.vec3.fromValues(0, 0, -1);
+        this.movementSpeed = SPEED;
+        this.mouseSensitivity = SENSITIVITY;
+        this.zoom = ZOOM;
+        this.position = position;
+        this.worldUp = up;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.updateCameraVectors();
+    }
+
+    getViewMatrix(){
+        let view = glMatrix.mat4.create();
+        let cameraTarget = glMatrix.vec3.create();
+        glMatrix.vec3.add(cameraTarget, this.position, this.front);
+        glMatrix.mat4.lookAt(view, this.position, cameraTarget, this.up);
+        return view;
+    }
+
+    updateCameraVectors() {
+        let front = glMatrix.vec3.create();
+        let x = Math.cos(glMatrix.glMatrix.toRadian(this.yaw)) * Math.cos(glMatrix.glMatrix.toRadian(this.pitch));
+        let y = Math.sin(glMatrix.glMatrix.toRadian(this.pitch));
+        let z = Math.sin(glMatrix.glMatrix.toRadian(this.yaw)) * Math.cos(glMatrix.glMatrix.toRadian(this.pitch));
+        glMatrix.vec3.set(front, x, y, z);
+        glMatrix.vec3.normalize(this.front, front);
+
+        // also re-calculate the Right and Up vector
+        // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        let right = glMatrix.vec3.create();
+        glMatrix.vec3.cross(right, this.front, this.worldUp);
+        glMatrix.vec3.normalize(this.right, right);
+
+        let up = glMatrix.vec3.create();
+        glMatrix.vec3.cross(up, this.right, this.front);
+        glMatrix.vec3.normalize(this.up, up);
+
+    }
+
+    onMousemove(offsetX, offsetY, constrainPitch = true) {
+        offsetX *= SENSITIVITY;
+        offsetY *= SENSITIVITY;
+
+        this.yaw += offsetX;
+        this.pitch += offsetY;
+
+        // make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (this.pitch > 89)
+            this.pitch = 89;
+        if (this.pitch < -89)
+            this.pitch = -89;
+        this.updateCameraVectors();
+    }
+
+    onMouseScroll(offsetY) {
+        this.zoom -= offsetY;
+        if (this.zoom < 1)
+            this.zoom = 1;
+        if (this.zoom > 45)
+            this.zoom = 45;
+    }
+
+    onKeydown(code,deltaTime){
+        let velocity = this.movementSpeed * deltaTime;
+        let cameraTarget = glMatrix.vec3.create();
+        if (code == "KeyW" || code =="ArrowUp") {
+            glMatrix.vec3.scale(cameraTarget, this.front, velocity);
+            glMatrix.vec3.add(this.position, this.position, cameraTarget);
+        } else if (code == "KeyS" || code == "ArrowUp") {
+            glMatrix.vec3.scale(cameraTarget, this.front, velocity);
+            glMatrix.vec3.subtract(this.position, this.position, cameraTarget);
+        } else if (code == "KeyA" || code == "ArrowLeft") {
+            glMatrix.vec3.cross(cameraTarget, this.front, this.worldUp);
+            glMatrix.vec3.normalize(cameraTarget, cameraTarget);
+            glMatrix.vec3.scale(cameraTarget, cameraTarget, velocity);
+            glMatrix.vec3.subtract(this.position, this.position, cameraTarget);
+        } else if (code == "KeyD" || code == "ArrowRight") {
+            glMatrix.vec3.cross(cameraTarget, this.front, this.worldUp);
+            glMatrix.vec3.normalize(cameraTarget, cameraTarget);
+            glMatrix.vec3.scale(cameraTarget, cameraTarget, velocity);
+            glMatrix.vec3.add(this.position, this.position, cameraTarget);
+        }
+    }
+
+}
