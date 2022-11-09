c67173d3e6909493d1da982a8947ff7140ece633
krapnik <krapnik@qq.com>
2022-11-10 00:20:46 +0800

    update

---
+++ 4_advance_opengl/11.1_anti_aliasing_msaa/main.js
@@ -0,0 +1,154 @@
+let cameraPos = glMatrix.vec3.fromValues(0.0, 0.0, 3.0);
+let camera = new Camera(cameraPos);
+
+const SCR_WIDTH = 800;
+const SCR_HEIGHT = 600;
+
+let deltaTime = 0.0;	// time between current frame and last frame
+let lastFrame = 0.0;
+let isFirstMouse = true;
+let lastX = SCR_WIDTH / 2, lastY = SCR_HEIGHT / 2;
+
+async function main() {
+    let stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    const gl = document.getElementById("canvas").getContext("webgl2",{"antialias":true});
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    let shader = new Shader(gl, "shader.vs", "shader.fs");
+    await shader.initialize();
+
+    let cubeVertices = new Float32Array([
+        // positions          // texture Coords        
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+
+        -0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        -0.5, -0.5, 0.5,
+
+        -0.5, 0.5, 0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, 0.5,
+        -0.5, 0.5, 0.5,
+
+        0.5, 0.5, 0.5,
+        0.5, 0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, 0.5,
+        0.5, 0.5, 0.5,
+
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+        -0.5, -0.5, 0.5,
+        -0.5, -0.5, -0.5,
+
+        -0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        -0.5, 0.5, -0.5
+    ]);
+
+    let positionLoc = 0;
+
+    let cubeVAO = gl.createVertexArray();
+    let cubeVBO = gl.createBuffer();
+
+    gl.bindVertexArray(cubeVAO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 3 * cubeVertices.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(positionLoc);
+    gl.bindVertexArray(null);
+
+    shader.use();
+
+    addGUI(gl);
+
+    function render(time) {
+        let currentFrame = Math.round(time) / 1000;
+        deltaTime = Math.floor(currentFrame * 1000 - lastFrame * 1000) / 1000;
+        lastFrame = currentFrame;
+        
+        gl.clearColor(0.2, 0.3, 0.3, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+        let model = glMatrix.mat4.identity(glMatrix.mat4.create());
+        let view = camera.getViewMatrix();;
+        let projection = glMatrix.mat4.create();
+        glMatrix.mat4.perspective(projection, glMatrix.glMatrix.toRadian(45), gl.canvas.width / gl.canvas.height, 0.1, 100)
+        
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        shader.setMat4("model", model);
+        gl.bindVertexArray(cubeVAO);
+        gl.drawArrays(gl.TRIANGLES, 0, 36);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+
+    let moveLock = true;
+    document.onkeydown = (e) => {
+        camera.onKeydown(e.code, deltaTime);
+
+        if (e.code == "Escape") {
+            moveLock = true;
+        }
+    }
+
+    canvas.onclick = (e) => {
+        if (moveLock) {
+            moveLock = false;
+        } else {
+            moveLock = true;
+        }
+        isFirstMouse = true;
+    }
+
+    canvas.onmousemove = (e) => {
+        if (moveLock) {
+            return;
+        }
+        let { clientX, clientY } = e;
+        if (isFirstMouse) {
+            lastX = clientX;
+            lastY = clientY;
+            isFirstMouse = false;
+        }
+        let offsetX = clientX - lastX;
+        let offsetY = lastY - clientY;
+
+        lastX = clientX;
+        lastY = clientY;
+
+        camera.onMousemove(offsetX, offsetY);
+    }
+
+    canvas.onwheel = (e) => {
+        camera.onMouseScroll(e.deltaY / 100);
+    }
+
+    function addGUI(gl) {
+        
+
+    }
+}
+
---
+++ 4_advance_opengl/11.2_anti_aliasing_offscreen/main.js
@@ -0,0 +1,243 @@
+let cameraPos = glMatrix.vec3.fromValues(0.0, 0.0, 3.0);
+let camera = new Camera(cameraPos);
+
+const SCR_WIDTH = 800;
+const SCR_HEIGHT = 600;
+
+let deltaTime = 0.0;	// time between current frame and last frame
+let lastFrame = 0.0;
+let isFirstMouse = true;
+let lastX = SCR_WIDTH / 2, lastY = SCR_HEIGHT / 2;
+
+async function main() {
+    let stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    const gl = document.getElementById("canvas").getContext("webgl2", { "antialias": false });
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    let shader = new Shader(gl, "shader.vs", "shader.fs");
+    await shader.initialize();
+
+    let screenShader = new Shader(gl, "anti_alias_post.vs", "anti_alias_post.fs");
+    await screenShader.initialize();
+
+    let cubeVertices = new Float32Array([
+        // positions          // texture Coords        
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+
+        -0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        -0.5, -0.5, 0.5,
+
+        -0.5, 0.5, 0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, 0.5,
+        -0.5, 0.5, 0.5,
+
+        0.5, 0.5, 0.5,
+        0.5, 0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, 0.5,
+        0.5, 0.5, 0.5,
+
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+        -0.5, -0.5, 0.5,
+        -0.5, -0.5, -0.5,
+
+        -0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        -0.5, 0.5, -0.5
+    ]);
+
+    let quadVertices = new Float32Array([   // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0, 1.0, 0.0, 1.0,
+        -1.0, -1.0, 0.0, 0.0,
+        1.0, -1.0, 1.0, 0.0,
+
+        -1.0, 1.0, 0.0, 1.0,
+        1.0, -1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0, 1.0
+    ]);
+
+    let positionLoc = 0, texCoordLoc = 1;
+
+    let cubeVAO = gl.createVertexArray();
+    let cubeVBO = gl.createBuffer();
+
+    gl.bindVertexArray(cubeVAO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 3 * cubeVertices.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(positionLoc);
+    gl.bindVertexArray(null);
+
+    // setup screen VAO
+    let quadVAO = gl.createVertexArray();
+    let quadVBO = gl.createBuffer();
+
+    gl.bindVertexArray(quadVAO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, quadVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, quadVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 4 * quadVertices.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(positionLoc);
+    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 4 * quadVertices.BYTES_PER_ELEMENT, 2 * quadVertices.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(texCoordLoc);
+    gl.bindVertexArray(null);
+
+    // configure MSAA framebuffer
+    // --------------------------
+    let renderFBO = gl.createFramebuffer();
+    // create a color attachment texture
+    // let textureColorBufferMultiSampled = gl.createTexture();
+    // gl.bindTexture(gl.TEXTURE_2D, textureColorBufferMultiSampled);
+    // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, SCR_WIDTH, SCR_HEIGHT, 0, gl.RGB, gl.UNSIGNED_BYTE, null);
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    // gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, textureColorBufferMultiSampled, 0);
+    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    let rbo = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, rbo);
+    gl.renderbufferStorageMultisample(gl.RENDERBUFFER,4, gl.RGBA8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, renderFBO);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE)
+        console.log("ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    // configure second post-processing framebuffer
+    let colorFBO = gl.createFramebuffer();
+    
+    // create a color attachment texture
+    let screenTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, screenTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, SCR_WIDTH, SCR_HEIGHT, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, colorFBO);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, screenTexture, 0);
+    
+    if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE)
+        console.log("ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    function render(time) {
+        let currentFrame = Math.round(time) / 1000;
+        deltaTime = Math.floor(currentFrame * 1000 - lastFrame * 1000) / 1000;
+        lastFrame = currentFrame;
+        // pass 1
+        // 1. draw scene as normal in multisampled buffers
+        gl.bindFramebuffer(gl.FRAMEBUFFER, renderFBO);
+        // gl.clearColor(0.1, 0.1, 0.1, 1.0);
+        // gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearBufferfv(gl.COLOR, 0, [0.1, 0.1, 0.1, 1.0]);
+        // gl.enable(gl.DEPTH_TEST);
+
+        // set transformation matrices		
+        shader.use();
+        let model = glMatrix.mat4.identity(glMatrix.mat4.create());
+        let view = camera.getViewMatrix();;
+        let projection = glMatrix.mat4.create();
+        glMatrix.mat4.perspective(projection, glMatrix.glMatrix.toRadian(45), gl.canvas.width / gl.canvas.height, 0.1, 100)
+
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        shader.setMat4("model", model);
+
+        gl.bindVertexArray(cubeVAO);
+        gl.drawArrays(gl.TRIANGLES, 0, 36);
+        gl.bindVertexArray(null);
+
+        // 2. now blit multisampled buffer(s) to normal colorbuffer of intermediate FBO. Image is stored in screenTexture
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, renderFBO);
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, colorFBO);
+        gl.blitFramebuffer(0, 0, SCR_WIDTH, SCR_HEIGHT, 0, 0, SCR_WIDTH, SCR_HEIGHT, gl.COLOR_BUFFER_BIT, gl.NEAREST);
+
+        // pass 2
+        // 3. now render quad with scene's visuals as its texture image
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.clearBufferfv(gl.COLOR, 0, [0.1, 0.1, 0.1, 1.0]);
+        
+        screenShader.use();
+        gl.bindVertexArray(quadVAO);
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, screenTexture); // use the now resolved color attachment as the quad's texture
+        
+        gl.clearColor(1.0, 1.0, 1.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        gl.bindVertexArray(null);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+
+    let moveLock = true;
+    document.onkeydown = (e) => {
+        camera.onKeydown(e.code, deltaTime);
+
+        if (e.code == "Escape") {
+            moveLock = true;
+        }
+    }
+
+    canvas.onclick = (e) => {
+        if (moveLock) {
+            moveLock = false;
+        } else {
+            moveLock = true;
+        }
+        isFirstMouse = true;
+    }
+
+    canvas.onmousemove = (e) => {
+        if (moveLock) {
+            return;
+        }
+        let { clientX, clientY } = e;
+        if (isFirstMouse) {
+            lastX = clientX;
+            lastY = clientY;
+            isFirstMouse = false;
+        }
+        let offsetX = clientX - lastX;
+        let offsetY = lastY - clientY;
+
+        lastX = clientX;
+        lastY = clientY;
+
+        camera.onMousemove(offsetX, offsetY);
+    }
+
+    canvas.onwheel = (e) => {
+        camera.onMouseScroll(e.deltaY / 100);
+    }
+}
+
