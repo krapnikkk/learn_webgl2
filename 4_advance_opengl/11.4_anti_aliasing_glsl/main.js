49eb04d1bcea67dcb8ec4e64843f00a53e4744c9
krapnik <krapnik@qq.com>
2022-11-10 22:18:35 +0800

    add anti_aliasing_glsl

---
+++ 4_advance_opengl/11.4_anti_aliasing_glsl/main.js
@@ -0,0 +1,118 @@
+async function main() {
+    let stats = new Stats();
+    document.body.appendChild(stats.dom);
+    var framesbuffer = {
+        switch: true
+    };
+    const SCR_WIDTH = 800;
+    const SCR_HEIGHT = 600;
+
+    const gl = document.getElementById("canvas").getContext("webgl2",{
+        width:SCR_WIDTH,
+        height:SCR_HEIGHT,
+        antialias: false
+    });
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    let shader = new Shader(gl, "shader.vs", "shader.fs");
+    await shader.initialize();
+    shader.use()
+    shader.setVec2("iResolution", [SCR_WIDTH,SCR_HEIGHT]);
+    shader.setBool("enabled",framesbuffer.switch);
+
+    let vertices = new Float32Array([
+        // positions          // colors           // texture coords
+        1, 1, 0.0, // top right
+        1, -1, 0.0,  // bottom right
+        -1, -1, 0.0, // bottom left
+        -1, 1, 0.0,   // top left 
+    ]);
+
+    let indices = new Uint8Array([
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    ]);
+
+    let positionLoc = 0, colorLoc = 1, texCoordLoc = 2;
+
+    let vao = gl.createVertexArray();
+    let vbo = gl.createBuffer();
+    let ebo = gl.createBuffer();
+
+    gl.bindVertexArray(vao);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 3 * vertices.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    // gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 8 * vertices.BYTES_PER_ELEMENT, 3 * vertices.BYTES_PER_ELEMENT);
+    // gl.enableVertexAttribArray(colorLoc);
+
+    // gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 8 * vertices.BYTES_PER_ELEMENT, 6 * vertices.BYTES_PER_ELEMENT);
+    // gl.enableVertexAttribArray(texCoordLoc);
+
+    let texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0 + 0);
+    // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+
+    let img = await loadImage("../../resources/textures/screen.png");
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    function render(time) {
+        gl.clearColor(0.2, 0.3, 0.3, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        shader.use();
+
+        gl.bindVertexArray(vao);
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 0);
+
+        requestAnimationFrame(render);
+        stats.update();
+    }
+
+    requestAnimationFrame(render);
+    addGUI();
+
+    function addGUI() {
+        const GUI = new dat.GUI({ name: "framesbuffer" });
+        GUI.add(framesbuffer, "switch").name("FXAA").onChange((val)=>{
+            console.log(val);
+            shader.setBool("enabled",val);
+        })
+    }
+}
+
+async function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        let image = new Image();
+        image.src = url;
+        image.onload = () => {
+            resolve(image);
+        }
+        image.onerror = (err) => {
+            reject(err);
+            throw new Error(err);
+        }
+
+    })
+}
